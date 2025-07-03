4716c9554119e074116841ae4fb59db5ac6c7088
Yin Xiaoyu <yinxiaoyu39999@gmail.com>
2025-07-03 23:48:46 +0800

    Version 1.6.0 add player highlight and refine scoring system

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ EMS Pool Gamble Application
 """
 
 # 应用版本信息
-APP_VERSION = "v1.5.1"
+APP_VERSION = "v1.6.0"
 APP_NAME = "EMS Pool Gamble"
-VERSION_DATE = "2025-06-30"
+VERSION_DATE = "2025-07-03"
 
 # 默认分数选项（包含特殊分数14和20）
 DEFAULT_SCORE_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 20]
